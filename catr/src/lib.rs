0d9d42df66ece3e1837fc6cbe09f2c4350f58e20
Ndimah Mike <ndimah22@protonmail.com>
2024-08-27 00:23:03 +0100

    feat: implement the cat command passing all the tests

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 use clap::{value_parser, Arg, ArgAction, Command};
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-type MyResult<T> = Result<T, Box<dyn Error>>;
+pub type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
@@ -9,9 +11,23 @@ pub struct Config {
     number_lines: bool,
     number_nonblank_lines: bool
 }
+struct ParsedFile {
+    buffer: Box<dyn BufRead>,
+    allow_blank: bool,
+    show_lines: bool
+}
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(buffer) => read_lines(ParsedFile {
+                buffer,
+                allow_blank: config.number_nonblank_lines,
+                show_lines: config.number_lines
+            })
+        }
+    }
     Ok(())
 }
 
@@ -29,17 +45,18 @@ pub fn get_args() -> MyResult<Config> {
             .value_parser(value_parser!(String))
         )
         .arg(
-            Arg::new("show_blanks")
+            Arg::new("count_blanks")
             .short('b')
-            .long("blanks")
+            .long("number-nonblank")
             .action(ArgAction::SetFalse)
-            .help("Print non-blank lines")
+            .help("Don't Print non-blank lines number")
         )
         .arg(
             Arg::new("show_line_number")
             .short('n')
             .long("number")
             .action(ArgAction::SetTrue)
+            .conflicts_with("count_blanks")
             .help("Print line numbers")
         ).get_matches();
     Ok(Config {
@@ -49,6 +66,28 @@ pub fn get_args() -> MyResult<Config> {
             .map(|s| s.as_str().to_string())
             .collect(),
         number_lines: matches.get_flag("show_line_number"),
-        number_nonblank_lines: matches.get_flag("show_blanks")
+        number_nonblank_lines: matches.get_flag("count_blanks")
     })
+}
+
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>>{
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
+}
+fn read_lines(file: ParsedFile) {
+    let mut i = 1;
+    for line in file.buffer.lines().map(|x| x.unwrap()) {
+        if !file.show_lines && file.allow_blank {
+            println!("{}", line);
+            continue;
+        }
+        if !file.allow_blank && line.trim().is_empty() {
+            println!("{}", line);
+        } else {
+            println!("{:>6}\t{}", i, line);
+            i += 1;
+        }
+    }
 }
--- catr/tests/cli.rs
+++ catr/tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use rand::{distributions::Alphanumeric, Rng};
 use std::fs;
+use catr::MyResult;
 
 const PRG: &str = "catr";
 const EMPTY: &str = "tests/inputs/empty.txt";
@@ -9,3 +10,193 @@ const FOX: &str = "tests/inputs/fox.txt";
 const SPIDERS: &str = "tests/inputs/spiders.txt";
 const BUSTLE: &str = "tests/inputs/the-bustle.txt";
 
+// --------------------------------------------------
+#[test]
+fn usage() -> MyResult<()> {
+    for flag in &["-h", "--help"] {
+        Command::cargo_bin(PRG)?
+            .arg(flag)
+            .assert()
+            .stdout(predicate::str::contains("Usage"));
+    }
+    Ok(())
+}
+
+// --------------------------------------------------
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+
+// --------------------------------------------------
+#[test]
+fn skips_bad_file() -> MyResult<()> {
+    let bad = gen_bad_file();
+    let expected = format!("{bad}: .* [(]os error 2[)]");
+    Command::cargo_bin(PRG)?
+        .arg(&bad)
+        .assert()
+        .success()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+
+// --------------------------------------------------
+fn run(args: &[&str], expected_file: &str) -> MyResult<()> {
+    let expected = fs::read_to_string(expected_file)?;
+    let output = Command::cargo_bin(PRG)?.args(args).output().unwrap();
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, expected);
+
+    Ok(())
+}
+
+// --------------------------------------------------
+fn run_stdin(
+    input_file: &str,
+    args: &[&str],
+    expected_file: &str,
+) -> MyResult<()> {
+    let input = fs::read_to_string(input_file)?;
+    let expected = fs::read_to_string(expected_file)?;
+    let output = Command::cargo_bin(PRG)?
+        .write_stdin(input)
+        .args(args)
+        .output()
+        .unwrap();
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, expected);
+    Ok(())
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle_stdin() -> MyResult<()> {
+    run_stdin(BUSTLE, &["-"], "tests/expected/the-bustle.txt.stdin.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle_stdin_n() -> MyResult<()> {
+    run_stdin(
+        BUSTLE,
+        &["-n", "-"],
+        "tests/expected/the-bustle.txt.n.stdin.out",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle_stdin_b() -> MyResult<()> {
+    run_stdin(
+        BUSTLE,
+        &["-b", "-"],
+        "tests/expected/the-bustle.txt.b.stdin.out",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn empty() -> MyResult<()> {
+    run(&[EMPTY], "tests/expected/empty.txt.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn empty_n() -> MyResult<()> {
+    run(&["-n", EMPTY], "tests/expected/empty.txt.n.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn empty_b() -> MyResult<()> {
+    run(&["-b", EMPTY], "tests/expected/empty.txt.b.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn fox() -> MyResult<()> {
+    run(&[FOX], "tests/expected/fox.txt.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn fox_n() -> MyResult<()> {
+    run(&["-n", FOX], "tests/expected/fox.txt.n.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn fox_b() -> MyResult<()> {
+    run(&["-b", FOX], "tests/expected/fox.txt.b.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn spiders() -> MyResult<()> {
+    run(&[SPIDERS], "tests/expected/spiders.txt.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn spiders_n() -> MyResult<()> {
+    run(&["--number", SPIDERS], "tests/expected/spiders.txt.n.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn spiders_b() -> MyResult<()> {
+    run(
+        &["--number-nonblank", SPIDERS],
+        "tests/expected/spiders.txt.b.out",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle() -> MyResult<()> {
+    run(&[BUSTLE], "tests/expected/the-bustle.txt.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle_n() -> MyResult<()> {
+    run(&["-n", BUSTLE], "tests/expected/the-bustle.txt.n.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn bustle_b() -> MyResult<()> {
+    run(&["-b", BUSTLE], "tests/expected/the-bustle.txt.b.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn all() -> MyResult<()> {
+    run(&[FOX, SPIDERS, BUSTLE], "tests/expected/all.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn all_n() -> MyResult<()> {
+    run(&[FOX, SPIDERS, BUSTLE, "-n"], "tests/expected/all.n.out")
+}
+
+// --------------------------------------------------
+#[test]
+fn all_b() -> MyResult<()> {
+    run(&[FOX, SPIDERS, BUSTLE, "-b"], "tests/expected/all.b.out")
+}
