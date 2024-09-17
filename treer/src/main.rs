9b3c154bd2a8fe561757051edff56013a6b607ce
Ndimah Mike <ndimah22@protonmail.com>
2024-09-17 15:35:45 +0100

    feat: setup the tree command implementation

---
+++ treer/src/main.rs
@@ -0,0 +1,8 @@
+use treer::{get_args, run};
+
+fn main() {
+    if let Err(e) =  get_args().and_then(run){
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
---
+++ treer/tests/cli.rs
@@ -0,0 +1,71 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use rand::{distributions::Alphanumeric, Rng};
+use std::{borrow::Cow, fs};
+use treer::MyResult;
+
+const PRG: &str = "treer";
+
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(8)
+            .map(char::from)
+            .collect();
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+#[cfg(windows)]
+fn format_file_name(file: &str) -> Cow<str> {
+    format!("{}.windows", file).into()
+}
+#[cfg(not(windows))]
+fn format_file_name(file: &str) -> Cow<str> {
+    file.into()
+}
+fn run(args: &[&str], expected_file: &str) -> MyResult<()> {
+    let file_name = format_file_name(expected_file);
+    let contents = fs::read_to_string(file_name.as_ref())?;
+    let mut expected: Vec<&str> = contents.split("\n").filter(|s| !s.is_empty()).collect();
+    expected.sort();
+    let cmd = Command::cargo_bin(PRG)?.args(args).assert().success();
+    let stdout = String::from_utf8(cmd.get_output().stdout.clone())?;
+    let mut lines: Vec<&str> = stdout.split("\n").filter(|s| !s.is_empty()).collect();
+    lines.sort();
+    assert_eq!(lines, expected);
+    Ok(())
+}
+
+#[test]
+fn skip_bad_dir() -> MyResult<()> {
+    let bad_file = gen_bad_file();
+    let expected = format!("{}: *. [(]os error [23][)]", &bad_file);
+    Command::cargo_bin(PRG)?
+        .arg(bad_file)
+        .assert()
+        .success()
+        .stderr(predicate::str::is_match(expected)?);
+    Ok(())
+}
+#[test]
+fn path1() -> MyResult<()> {
+    run(&["tests/inputs"], "tests/expected/path1.txt")
+}
+#[test]
+fn path_a() -> MyResult<()> {
+    run(&["tests/inputs/a"], "tests/expected/path_a.txt")
+}
+#[test]
+fn path_a_b() -> MyResult<()> {
+    run(&["tests/inputs/a/b"], "tests/expected/path_a_b.txt")
+}
+#[test]
+fn path_a_b_d() -> MyResult<()> {
+    run(
+        &["tests/inputs/a/b", "tests/inputs/d"],
+        "tests/expected/path_a_b_d.txt",
+    )
+}
