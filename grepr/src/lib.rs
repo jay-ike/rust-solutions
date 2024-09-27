2c6394faca78ae83f22ae9fe12b99a5907a590ae
Ndimah Mike <ndimah22@protonmail.com>
2024-09-27 03:54:39 +0100

    feat: passed the basic features of the grep command

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -1,4 +1,9 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+    mem,
+};
 
 use clap::{value_parser, Arg, ArgAction, Command};
 use regex::{Regex, RegexBuilder};
@@ -88,36 +93,96 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let entries = find_files(&config.files, config.recursive);
+    for entry in &entries {
+        match entry {
+            Err(e) => eprintln!("{}", e),
+            Ok(filename) => match open(&filename) {
+                Err(e) => eprintln!("{}: {}", filename, e),
+                Ok(file) => {
+                    let f = if entries.len() > 1 {
+                        format!("{}:", filename)
+                    } else {
+                        "".to_string()
+                    };
+                    let matches = find_lines(file, &config.pattern, config.invert_match)?;
+                    if config.count {
+                        print!("{}{}\n", f, matches.len());
+                    } else {
+                        matches.iter().for_each(|line| {
+                            print!("{}{}", f, line);
+                        })
+                    }
+                }
+            },
+        }
+    }
     Ok(())
 }
 
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn find_lines<T: BufRead>(
+    mut file: T,
+    pattern: &Regex,
+    invert_match: bool,
+) -> MyResult<Vec<String>> {
+    let mut matches = vec![];
+    let mut line = String::new();
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+        if pattern.is_match(line.as_str()) ^ invert_match {
+            matches.push(mem::take(&mut line));
+        }
+        line.clear();
+    }
+    Ok(matches)
+}
+
 pub fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
-    paths
-        .iter()
-        .flat_map(|s| {
-            let dir = if recursive {
-                WalkDir::new(&s).min_depth(1)
-            } else {
-                WalkDir::new(&s).min_depth(0).max_depth(0)
-            };
-            dir.into_iter().map(move |e| match e {
-                Err(_) => Err(format!("\"{}\": No such file or directory", &s).into()),
-                Ok(entry) => {
-                    if entry.file_type().is_dir() && !recursive {
-                        return Err(format!("{} is a directory", entry.path().display()).into());
+    let mut files = vec![];
+    for path in paths {
+        match path.as_str() {
+            "-" => files.push(Ok(path.to_string())),
+            _ => match std::fs::metadata(path) {
+                Err(e) => files.push(Err(format!("{}: {}", path, e).into())),
+                Ok(data) => {
+                    if data.is_dir() {
+                        if recursive {
+                            WalkDir::new(path)
+                                .sort_by_file_name()
+                                .into_iter()
+                                .flatten()
+                                .filter(|e| e.file_type().is_file())
+                                .map(|e| e.path().to_str().unwrap().to_string())
+                                .for_each(|s| files.push(Ok(s)));
+                        } else {
+                            files.push(Err(From::from(format!("{} is a directory", path))));
+                        }
+                    } else if data.is_file() {
+                        files.push(Ok(path.to_string()));
                     }
-                    Ok(entry.path().to_str().unwrap_or_default().to_string())
                 }
-            })
-        })
-        .collect()
+            },
+        }
+    }
+    files
 }
 
 #[cfg(test)]
 mod tests {
-    use super::find_files;
+    use super::{find_files, find_lines};
     use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
+    use std::io::Cursor;
     #[test]
     fn test_find_files() {
         // Verify that the function finds a file known to exist
@@ -156,5 +221,31 @@ mod tests {
         let files = find_files(&[bad], false);
         assert_eq!(files.len(), 1);
         assert!(files[0].is_err());
+    }
+    #[test]
+    fn test_find_lines() {
+        let text = b"Lorem\nIpsum\r\nDOLOR";
+        // The pattern _or_ should match the one line, "Lorem"
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+        // When inverted, the function should match the other two lines
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+        // This regex will be case-insensitive
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+        // The two lines "Lorem" and "DOLOR" should match
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+        // When inverted, the one remaining line should match
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
     }
 }
